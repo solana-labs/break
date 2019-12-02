6866d62cf25af7e984fbafff77a0a8a91cc8966c
igor_burak <igor.burak@zenio.co>
2019-12-02 17:35:51 +0300

    update game page add share buttons add popover

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -5,19 +5,18 @@ import {Link} from "react-router-dom";
 
 interface IProps {
     name: string
-    linkTo: string
+    linkTo?: string
     typeButton?: boolean
     typeALink?: boolean
     onClick?(): void
 }
 
 export const Button = ({name, linkTo, typeButton,typeALink,onClick}: IProps) => {
-
     return (
         <React.Fragment>
-            {!typeButton && !typeALink && <Link to={linkTo} className={'button-component'}>{name}</Link>}
-            {typeButton && <button onClick={onClick} className={'button-component'}>{name}</button>}
-            {typeALink && <a href={linkTo} className={'button-component'}>{name}</a>}
+            {!typeButton && !typeALink && <Link to={linkTo ? linkTo : '#'} className={'button-component'}><span>{name}</span></Link>}
+            {typeButton && <button onClick={onClick} className={'button-component'}><span>{name}</span></button>}
+            {typeALink && <a href={linkTo} className={'button-component'}><span>{name}</span></a>}
         </React.Fragment>
     );
 };
