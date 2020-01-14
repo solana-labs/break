934b65b2786219f0de064bd5bdeab5c92de620d3
Evgeny Shvingelsky <evgeny.shvingelsky@zensoft.io>
2020-01-14 17:19:05 +0300

    checking valid connection via blockhash

--- src/services/transactions-service/index.ts
+++ src/services/transactions-service/index.ts
@@ -2,16 +2,6 @@ import {ITransactionsService, TransactionInfoService} from "./model";
 import * as solanaWeb3 from '@solana/web3.js';
 import {sendAndConfirmRecentTransaction} from '@solana/web3.js';
 
-const arrayRpcAddress = [
-    '34.83.146.144:8899',
-    '34.83.236.72:8899',
-    '104.199.124.4:8899',
-    '34.82.125.134:8899',
-    '34.82.69.78:8899',
-    '18.209.228.98:8899',
-    '89.42.234.59:8899',
-    '89.42.234.25:8899',
-];
 
 export default class TransactionsService implements ITransactionsService {
     account: any;
@@ -19,23 +9,38 @@ export default class TransactionsService implements ITransactionsService {
     secretKey: any;
     connectionArray: any;
 
+    checkBlockHash = async (connection: any) => {
+        const blockHash = await connection.getRecentBlockhash();
+        const blockHash2 = await connection.getRecentBlockhash();
+
+        // checking if it is possible to get a new blockHash
+        if(blockHash[0] !== blockHash2[0]){
+            // if it's valid url connection
+            this.connectionArray.push(connection);
+        }
+    };
+
     setConnection = async () => {
         const {Account, Connection} = solanaWeb3;
 
         try {
+            const connection = new Connection('https://testnet.solana.com:8443/');
+            const clusterNodes = await connection.getClusterNodes();
+
             this.connectionArray = [];
 
-            arrayRpcAddress.forEach((rpc: string, index: number) => {
-                this.connectionArray[index] = new Connection(`http://${rpc}`);
+            clusterNodes.forEach((obj: any) => {
+                const connection = new Connection(`http://${obj.rpc}`);
+                this.checkBlockHash(connection);
             });
 
-            let secKey = localStorage.getItem('secretKey4');
+            let secKey = localStorage.getItem('secretKey');
 
             if (!secKey) {
                 const account = await new Account();
                 secKey = account._keypair.secretKey;
 
-                localStorage.setItem('secretKey4', JSON.stringify(secKey));
+                localStorage.setItem('secretKey', JSON.stringify(secKey));
 
                 this.secretKey = secKey;
 
@@ -51,7 +56,7 @@ export default class TransactionsService implements ITransactionsService {
 
             this.account = new Account(new Uint8Array(bufferArray));
 
-            const balance = await this.connectionArray[0].getBalance(this.account.publicKey);
+            const balance = await connection.getBalance(this.account.publicKey);
             //console.log('balance - ', balance);
 
         } catch (e) {
@@ -67,7 +72,7 @@ export default class TransactionsService implements ITransactionsService {
             lamportsCount: 1000
         };
 
-        const index = id % arrayRpcAddress.length; // get index of rpc array
+        const index = id % this.connectionArray.length; // get index of rpc array
         const connection = this.connectionArray[index];
 
         this.keypair = new Account();
