4f504e78a4aa83c3535f1a400fa2891e079f831d
Justin Starry <justin@solana.com>
2020-03-31 15:24:08 +0800

    Update web3 to v0.39.0

--- server/src/urls.ts
+++ server/src/urls.ts
@@ -1,16 +1,28 @@
 // To connect to a public testnet, set `export LIVE=1` in your
 // environment. By default, `LIVE=1` will connect to the stable testnet.
 
-import { testnetChannelEndpoint } from "@solana/web3.js";
+import { clusterApiUrl, Cluster } from "@solana/web3.js";
+
+function chooseCluster(): Cluster {
+  switch (process.env.CLUSTER) {
+    case "devnet":
+    case "testnet":
+    case "mainnet-beta": {
+      return process.env.CLUSTER;
+    }
+  }
+
+  return "devnet";
+}
 
 export const url =
   process.env.RPC_URL ||
   (process.env.LIVE
-    ? testnetChannelEndpoint(process.env.CHANNEL || "stable", false)
+    ? clusterApiUrl(chooseCluster(), false)
     : "http://localhost:8899");
 
 export const urlTls =
   process.env.RPC_URL ||
   (process.env.LIVE
-    ? testnetChannelEndpoint(process.env.CHANNEL || "stable", true)
+    ? clusterApiUrl(chooseCluster(), true)
     : "http://localhost:8899");
