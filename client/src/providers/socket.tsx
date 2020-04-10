f9c1a13f9e3833551f434860c3ed4be637cd8985
Justin Starry <justin@solana.com>
2020-04-10 21:56:34 +0800

    Show active users

--- client/src/providers/socket.tsx
+++ client/src/providers/socket.tsx
@@ -5,20 +5,28 @@ import { sleep } from "utils";
 type SetSocket = React.Dispatch<React.SetStateAction<WebSocket | undefined>>;
 const SocketContext = React.createContext<WebSocket | undefined>(undefined);
 
+type SetActiveUsers = React.Dispatch<React.SetStateAction<number>>;
+const ActiveUsersContext = React.createContext<number | undefined>(undefined);
+
 type SocketProviderProps = { children: React.ReactNode };
 export function SocketProvider({ children }: SocketProviderProps) {
   let [socket, setSocket] = React.useState<WebSocket | undefined>(undefined);
+  let [activeUsers, setActiveUsers] = React.useState<number>(1);
 
   React.useEffect(() => {
-    newSocket(setSocket);
+    newSocket(setSocket, setActiveUsers);
   }, []);
 
   return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={socket}>
+      <ActiveUsersContext.Provider value={activeUsers}>
+        {children}
+      </ActiveUsersContext.Provider>
+    </SocketContext.Provider>
   );
 }
 
-function newSocket(setSocket: SetSocket) {
+function newSocket(setSocket: SetSocket, setActiveUsers: SetActiveUsers) {
   const socket = new WebSocket(Path.WS);
   const timeoutId = setTimeout(() => {
     if (socket.readyState !== WebSocket.OPEN) {
@@ -27,11 +35,17 @@ function newSocket(setSocket: SetSocket) {
   }, 5000);
 
   socket.onopen = () => setSocket(socket);
+  socket.onmessage = e => {
+    const data = JSON.parse(e.data);
+    if ("activeUsers" in data) {
+      setActiveUsers(data.activeUsers);
+    }
+  };
   socket.onclose = async () => {
     await sleep(2000);
     clearTimeout(timeoutId);
     setSocket(undefined);
-    newSocket(setSocket);
+    newSocket(setSocket, setActiveUsers);
   };
   socket.onerror = async err => {
     console.error(err);
@@ -41,4 +55,13 @@ function newSocket(setSocket: SetSocket) {
 
 export function useSocket() {
   return React.useContext(SocketContext);
+}
+
+export function useActiveUsers() {
+  const context = React.useContext(ActiveUsersContext);
+  if (!context) {
+    throw new Error(`useActiveUsers must be used within a SocketProvider`);
+  }
+
+  return context;
 }
