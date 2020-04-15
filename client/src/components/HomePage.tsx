dad047fc800a14b5528d8765b7246e3f96f98b25
Justin Starry <justin@solana.com>
2020-04-15 21:18:22 +0800

    Fix typo

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
             <div className="col-12 col-lg-8">
               <p className="introduction my-5">
                 Solana is the world’s most performant blockchain. Currently it
-                can handle 50,000 transactions per seconds with 400 millisecond
+                can handle 50,000 transactions per second with 400 millisecond
                 block times. This is enough to handle the global capacity of
                 Visa. To see just how fast it is, let’s see if you can break it.
               </p>
