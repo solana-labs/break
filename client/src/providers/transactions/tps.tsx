1894bbb19851ca1b450122795eb7b02da041e95f
Justin Starry <justin@solana.com>
2020-05-05 15:16:56 +0800

    Fix TPS calculation when replaying

--- client/src/providers/transactions/tps.tsx
+++ client/src/providers/transactions/tps.tsx
@@ -7,34 +7,30 @@ const TPS_LOOK_BACK = 10;
 export const TpsContext = React.createContext<number | undefined>(undefined);
 type ProviderProps = { children: React.ReactNode };
 export function TpsProvider({ children }: ProviderProps) {
-  const recentCounts = React.useRef<number[]>([]);
   const [tps, setTps] = React.useState(0);
   const createdCount = useCreatedCount();
-  const createdCountRef = React.useRef(createdCount);
+  const createdCountRef = React.useRef(0);
+  createdCountRef.current = createdCount;
 
   React.useEffect(() => {
-    createdCountRef.current = createdCount;
-  }, [createdCount]);
-
-  React.useEffect(() => {
-    recentCounts.current = [];
+    const recentCounts: number[] = [];
     const timerId = setInterval(() => {
       if (createdCountRef.current === 0) {
-        recentCounts.current = [];
+        recentCounts.splice(0);
         setTps(0);
         return;
       }
 
-      recentCounts.current.push(createdCountRef.current);
-      while (recentCounts.current.length - 1 > TPS_LOOK_BACK) {
-        recentCounts.current.shift();
+      recentCounts.push(createdCountRef.current);
+      while (recentCounts.length - 1 > TPS_LOOK_BACK) {
+        recentCounts.shift();
       }
 
-      const ticksElapsed = recentCounts.current.length - 1;
+      const ticksElapsed = recentCounts.length - 1;
       if (ticksElapsed <= 0) return;
 
-      const oldTxCount = recentCounts.current[0];
-      const latestTxCount = recentCounts.current[ticksElapsed];
+      const oldTxCount = recentCounts[0];
+      const latestTxCount = recentCounts[ticksElapsed];
       const tps =
         (latestTxCount - oldTxCount) / ((TPS_REFRESH_MS / 1000) * ticksElapsed);
       setTps(Math.floor(tps));
