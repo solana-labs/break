fab71d4be794ce6540bfa61339ef5a88336171d5
Justin Starry <justin@solana.com>
2020-06-25 16:39:42 +0800

    Fix active client count

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -5,25 +5,21 @@ import TpuProxy from "./tpu_proxy";
 export default class WebSocketServer {
   static start(httpServer: http.Server, tpuProxy: TpuProxy): void {
     // Start websocket server
-    let activeClients = 0;
-    let lastNotifiedCount = 0;
+    let activeUsers = 0;
     const wss = new WebSocket.Server({ server: httpServer });
     wss.on("connection", function connection(ws) {
-      activeClients++;
-      ws.on("close", () => activeClients--);
+      activeUsers++;
+      ws.on("close", () => activeUsers--);
       ws.on("message", tpuProxy.onTransaction);
     });
 
     // Start active user broadcast loop
     setInterval(() => {
-      if (activeClients !== lastNotifiedCount) {
-        lastNotifiedCount = activeClients;
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ activeUsers: activeClients }));
-          }
-        });
-      }
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ activeUsers }));
+        }
+      });
     }, 1000);
   }
 }
