32c3f79b0b17e143d5a06a1f44fd0d3b3b942faa
Justin Starry <justin@solana.com>
2020-12-17 11:10:39 +0800

    Allow withdrawing funds to address

--- client/src/components/PaymentCard.tsx
+++ client/src/components/PaymentCard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import QRCode from "qrcode.react";
-import { Account, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import {
+  Account,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 
 import { useConfig, useRefreshAccounts } from "providers/server/http";
 import { useBalance } from "providers/rpc/balance";
 import { usePayerState } from "providers/wallet";
+import { useConnection } from "providers/rpc";
+import { useBlockhash } from "providers/rpc/blockhash";
 
 export function lamportsToSolString(
   lamports: number,
@@ -36,6 +45,8 @@ export function getTrustWalletLink(
 
 export function PaymentCard({ account }: { account: Account }) {
   const balance = useBalance();
+  const connection = useConnection();
+  const recentBlockhash = useBlockhash();
   const [, setPayer] = usePayerState();
   const gameCostLamports = useConfig()?.gameCost || 0;
   const gameCostSol = gameCostLamports / LAMPORTS_PER_SOL;
@@ -45,11 +56,59 @@ export function PaymentCard({ account }: { account: Account }) {
   const trustWalletDeepLink = getTrustWalletLink(address, gameCostSol);
   const [copied, setCopied] = React.useState(false);
   const [showQR, setShowQR] = React.useState(!trustWalletDeepLink);
+  const [showWithdraw, setShowWithdraw] = React.useState(false);
+  const [toPubkey, setToPubkey] = React.useState<PublicKey>();
+  const [withdrawMessage, setWithdrawMessage] = React.useState("");
 
   const copyAddress = () => {
     navigator.clipboard.writeText(address);
     setCopied(true);
   };
+
+  const updateToPubkey = (event: React.FormEvent<HTMLInputElement>) => {
+    setWithdrawMessage("");
+    try {
+      setToPubkey(new PublicKey(event.currentTarget.value));
+    } catch (err) {
+      setToPubkey(undefined);
+    }
+  };
+
+  const withdrawEnabled = React.useMemo(() => {
+    return (
+      connection && recentBlockhash && toPubkey && typeof balance === "number"
+    );
+  }, [connection, recentBlockhash, toPubkey, balance]);
+
+  const withdrawFunds = React.useCallback(() => {
+    if (
+      connection &&
+      recentBlockhash &&
+      toPubkey &&
+      typeof balance === "number"
+    ) {
+      (async () => {
+        try {
+          await sendAndConfirmTransaction(
+            connection,
+            new Transaction({ recentBlockhash }).add(
+              SystemProgram.transfer({
+                fromPubkey: account.publicKey,
+                toPubkey,
+                lamports: balance - 5000,
+              })
+            ),
+            [account],
+            { commitment: "singleGossip", preflightCommitment: "singleGossip" }
+          );
+          setWithdrawMessage("Withdraw succeeded");
+        } catch (err) {
+          console.error(err);
+          setWithdrawMessage("Withdraw failed");
+        }
+      })();
+    }
+  }, [account, balance, connection, recentBlockhash, toPubkey]);
 
   React.useEffect(() => {
     if (!copied) return;
@@ -126,6 +185,41 @@ export function PaymentCard({ account }: { account: Account }) {
             </h4>
           </span>
         </div>
+
+        <div className="d-flex align-items-center justify-content-between mb-4 pb-4 border-bottom">
+          <div className="font-weight-bold">Withdraw Funds</div>
+          <span
+            className="btn btn-sm btn-white"
+            onClick={() => {
+              setShowWithdraw((show) => !show);
+              setWithdrawMessage("");
+            }}
+          >
+            {showWithdraw ? "Hide" : "Withdraw"}
+          </span>
+        </div>
+
+        {showWithdraw && (
+          <div className="mb-4 pb-4 border-bottom">
+            {withdrawMessage && <p>{withdrawMessage}</p>}
+            <div className="d-flex align-items-center justify-content-between">
+              <input
+                type="text"
+                className="form-control"
+                style={{ flex: 1, maxWidth: "500px" }}
+                placeholder="To Address"
+                onInput={updateToPubkey}
+              />
+              <input
+                className="btn btn-sm btn-white ml-2"
+                type="button"
+                value="Withdraw"
+                disabled={!withdrawEnabled}
+                onClick={() => withdrawFunds()}
+              />
+            </div>
+          </div>
+        )}
 
         <div className="d-flex align-items-center justify-content-between">
           <div className="font-weight-bold">Change Wallet</div>
