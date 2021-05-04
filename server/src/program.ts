6702d911bfb9a116061a5e9381c9702b38b31f12
Justin Starry <justin@solana.com>
2021-05-04 15:36:37 +0800

    Remove unused supply code from server (#861)
    * Remove unused supply code from server
    
    * npm run format

--- server/src/program.ts
+++ server/src/program.ts
@@ -1,102 +1,30 @@
-import {
-  Account,
-  Connection,
-  PublicKey,
-  BpfLoader,
-  Transaction,
-  FeeCalculator,
-  SystemProgram,
-  sendAndConfirmTransaction,
-  BPF_LOADER_DEPRECATED_PROGRAM_ID,
-} from "@solana/web3.js";
+import { Account, PublicKey } from "@solana/web3.js";
 import path from "path";
-import _fs from "fs";
-import Faucet from "./faucet";
-import { reportError, sleep } from "./utils";
-const fs = _fs.promises;
+import fs from "fs";
 
-const ENCODED_PROGRAM_KEY = process.env.ENCODED_PROGRAM_KEY;
 const DEPLOYED_PROGRAM_ADDRESS = process.env.DEPLOYED_PROGRAM_ADDRESS;
 
-export default class ProgramLoader {
-  private static programAccount(): Account {
-    if (ENCODED_PROGRAM_KEY) {
-      return new Account(Buffer.from(ENCODED_PROGRAM_KEY, "base64"));
-    } else {
-      return new Account();
-    }
+const PROGRAM_KEYPAIR_PATH = path.resolve(
+  "..",
+  "program",
+  "target",
+  "deploy",
+  "break_solana_program-keypair.json"
+);
+
+export const PROGRAM_ID = (() => {
+  if (DEPLOYED_PROGRAM_ADDRESS) {
+    return new PublicKey(DEPLOYED_PROGRAM_ADDRESS);
+  } else {
+    return readAccountFromFile(PROGRAM_KEYPAIR_PATH).publicKey;
   }
+})();
 
-  private static programAddress(): PublicKey {
-    if (DEPLOYED_PROGRAM_ADDRESS) {
-      return new PublicKey(DEPLOYED_PROGRAM_ADDRESS);
-    } else {
-      return ProgramLoader.programAccount().publicKey;
-    }
-  }
-
-  static async load(
-    connection: Connection,
-    faucet: Faucet,
-    feeCalculator: FeeCalculator
-  ): Promise<PublicKey> {
-    const programAddress = ProgramLoader.programAddress();
-    const programAccount = ProgramLoader.programAccount();
-
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      try {
-        // If the program account already exists, don't try to load it again
-        const info = (await connection.getParsedAccountInfo(programAddress))
-          .value;
-        if (info?.executable) return programAddress;
-
-        const NUM_RETRIES = 100; /* allow some number of retries */
-        const elfFile = path.join(
-          __dirname,
-          "..",
-          "..",
-          "program",
-          "dist",
-          "break_solana_program.so"
-        );
-        console.log(`Reading ${elfFile}...`);
-        const elfData = await fs.readFile(elfFile);
-
-        console.log("Loading break solana program...");
-        const fees =
-          feeCalculator.lamportsPerSignature *
-            (BpfLoader.getMinNumSignatures(elfData.length) + NUM_RETRIES) +
-          (await connection.getMinimumBalanceForRentExemption(elfData.length));
-
-        const loaderAccount = new Account();
-        await sendAndConfirmTransaction(
-          connection,
-          new Transaction().add(
-            SystemProgram.transfer({
-              fromPubkey: faucet.feeAccount.publicKey,
-              toPubkey: loaderAccount.publicKey,
-              lamports: fees,
-            })
-          ),
-          [faucet.feeAccount],
-          { commitment: "single", skipPreflight: true }
-        );
-
-        await BpfLoader.load(
-          connection,
-          loaderAccount,
-          programAccount,
-          elfData,
-          BPF_LOADER_DEPRECATED_PROGRAM_ID
-        );
-        console.log("Program Loaded");
-        break;
-      } catch (err) {
-        reportError(err, "Failed to load program");
-        await sleep(1000);
-      }
-    }
-    return programAccount.publicKey;
-  }
+/**
+ * Create an Account from a keypair file
+ */
+function readAccountFromFile(filePath: string): Account {
+  const keypairString = fs.readFileSync(filePath, { encoding: "utf8" });
+  const keypairBuffer = Buffer.from(JSON.parse(keypairString));
+  return new Account(keypairBuffer);
 }
