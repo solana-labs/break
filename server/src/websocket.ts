91ac68c02e308206d633c60fcbbb1ff3920396a0
Justin Starry <justin@solana.com>
2021-07-16 23:56:14 -0500

    Update ws and time out clients

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -2,15 +2,34 @@ import WebSocket from "ws";
 import http from "http";
 import TpuProxy from "./tpu_proxy";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function noop() {}
+
 export default class WebSocketServer {
   static start(httpServer: http.Server, tpuProxy: TpuProxy): void {
     // Start websocket server
     let activeUsers = 0;
     const wss = new WebSocket.Server({ server: httpServer });
     wss.on("connection", function connection(ws) {
+      let isAlive = true;
+      function heartbeat() {
+        console.log("heartbeat");
+        isAlive = true;
+      }
+
+      const interval = setInterval(function ping() {
+        if (isAlive === false) return ws.terminate();
+        isAlive = false;
+        ws.ping(noop);
+      }, 30000);
+
       activeUsers++;
-      ws.on("close", () => activeUsers--);
+      ws.on("close", () => {
+        clearInterval(interval);
+        activeUsers--;
+      });
       ws.on("message", tpuProxy.onTransaction);
+      ws.on("pong", heartbeat);
     });
 
     // Start active user broadcast loop
